f6e4f977dc534e5583a0e589dcf5ffa1107553a2
SourceSensei <source.sensei1205@gmail.com>
2023-12-03 19:11:22 +0000

    first commit

---
+++ src/components/Hero.svelte
@@ -0,0 +1,66 @@
+<script>
+	import { onMount, onDestroy } from 'svelte';
+	import { Map } from 'mapbox-gl';
+	import 'mapbox-gl/dist/mapbox-gl.css';
+
+	let map;
+	let mapContainer;
+	let lng, lat, zoom;
+
+	lng = -71.224518;
+	lat = 42.213995;
+	zoom = 9;
+
+	function updateData() {
+		zoom = map.getZoom();
+		lng = map.getCenter().lng;
+		lat = map.getCenter().lat;
+	}
+
+	onMount(() => {
+		const initialState = { lng: lng, lat: lat, zoom: zoom };
+
+		map = new Map({
+			container: mapContainer,
+			accessToken:
+				'pk.eyJ1Ijoic291cmNlc2Vuc2VpIiwiYSI6ImNscG90MGhuNzBsMGMydW81Nmk0c3luZ2IifQ.nVxhKgD2mqEzy9j7WQhZGQ',
+			style: `mapbox://styles/mapbox/outdoors-v11`,
+			center: [initialState.lng, initialState.lat],
+			zoom: initialState.zoom
+		});
+
+		map.on('move', () => {
+			updateData();
+		});
+	});
+
+	onDestroy(() => {
+		map.remove();
+	});
+</script>
+
+<section class="text-stone-950">
+	<div
+		class="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-3xl"
+	>
+		
+			<h1 class="text-4xl font-bold leadi sm:text-5xl">MapBox Playground</h1>
+		
+
+			<div class="map" bind:this={mapContainer} />
+		
+		<div class="flex flex-wrap justify-center">
+			<button class="px-8 py-3 m-2 text-lg border rounded text-stone-950 border-gray-700"
+				>Learn more</button
+			>
+		</div>
+	</div>
+</section>
+
+<style>
+	.map {
+		width: 500px;
+		height: 500px;
+		margin: 2rem;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Nav from './../components/Nav.svelte';
+	import Hero from '../components/Hero.svelte';
+	import '../styles/global.css';
+</script>
+
+<Nav />
+<Hero />
+
